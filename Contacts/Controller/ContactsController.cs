c58fc7c582780da2750bb9f8315aed16bf179f70
Michał Ptasznik <michal.ptasznik@protonmail.com>
2026-04-18 09:48:12 +0200

    Docker compose setup

---
+++ Contacts/Controller/ContactsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Contacts.Controller
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ContactsController : ControllerBase
+    {
+        [HttpGet]
+        [Route("alive")]
+        public IActionResult IsAlive()
+        {
+            return Ok("Contacts API is alive!");
+        }
+    }
+}
