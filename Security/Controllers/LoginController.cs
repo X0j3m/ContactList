56ac57c8312c8202108c4538446845cf828b25b5
Michał Ptasznik <michal.ptasznik@protonmail.com>
2026-04-19 13:07:17 +0200

    Fixed issue with no connecting to Security microservice

---
+++ Security/Controllers/LoginController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Security.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LoginController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult GetJwtToken()
+        {
+            var dummyJwtToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
+            return Ok(new { token = dummyJwtToken });
+        }
+    }
+}
